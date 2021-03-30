f987e6cc5beae06bf165ae13ee5aa78346c7ffd9
alexandraantohi <73305226+alexandraantohi@users.noreply.github.com>
2021-03-30 09:33:52 +0300

    Added interaction with Yelp API

--- Ravenous/ravenous/src/App.js
+++ Ravenous/ravenous/src/App.js
@@ -2,15 +2,45 @@ import React, { Component } from "react"
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from "./components/BusinessList/BusinessList"
+import Yelp from "./util/Yelp"
 import SearchBar from "./components/SearchBar/SearchBar"
 
+// const business = {
+//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// }
+
+// const businesses = [business, business, business, business, business, business]
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this)
+  }
+
+  searchYelp(term, location, sortBy) {
+    //console.log(`You are searching for ${term}, ${location} and ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState({ businesses: businesses })
+    }); 
+  }
+
   render(){
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses}/>
       </div>
     );
   }
